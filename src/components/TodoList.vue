67bddfd8d3d9124937447ceb162d2d91b7dc4eb6
lee <lsljw65@gmail.com>
2023-12-15 15:30:47 +0900

    3rd input-5 할일목록 만들

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,29 @@
 <template>
-    <div>
-        목록
-    </div>
+    <section>
+        <ul>
+            <li v-for="todoItem in todoItems">{{ todoItem }}</li>
+        </ul>
+    </section>
 </template>
 <script>
 export default{
-
+    data(){
+        return{
+            todoItems:[]
+        }
+    },
+    created(){
+        if(localStorage.length > 0){
+            for (var i=0; i<localStorage.length; i++){
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    }
 }
 </script>
 
 <style>
-
+li{
+    display:block;
+}
 </style>
