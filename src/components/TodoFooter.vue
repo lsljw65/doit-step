e38370c5327f1c49a7face1d8c586025ddd080bc
lee <lsljw65@gmail.com>
2023-12-15 16:30:21 +0900

    4th footer 모두 삭제하

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,14 +1,29 @@
 <template>
-    <div>
-        푸터
+    <div class="clearAllContainer">
+        <span class="clearAllBtn" @click="clearTodo">Clear All</span>
     </div>
 </template>
 <script>
 export default{
-
+    methods:{
+        clearTodo(){
+            localStorage.clear();
+        }
+    }
 }
 </script>
 
 <style>
-
+    .clearAllContainer{
+        width:8.5rem;
+        height:50px;
+        line-height:50px;
+        background-color:white;
+        border-radius:5px;
+        margin:10px auto;
+    }
+    .clearAllBtn{
+        color:#e20303;
+        display:black;
+    }
 </style>
